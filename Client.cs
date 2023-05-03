6843e81a3de46d70f3333d53af11acc0d13b42d8
Sahab Masrour <sahab.masroor@gmail.com>
2023-05-03 14:42:09 +0430

    Single client server socket communication implemented

---
+++ Client.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        public const int PortNumber = 11111;
+
+        static void Main(string[] args)
+        {
+            ExecuteClient();
+        }
+
+        private static void ExecuteClient()
+        {
+            IPHostEntry hostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = hostEntry.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 11111);
+
+            Socket clientSocket = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                clientSocket.Connect(endPoint);
+                // send ======================
+                byte[] message = Encoding.ASCII.GetBytes("Hello networking world");
+
+                var sizeSent = clientSocket.Send(message);
+
+                // received
+                byte[] byteReceived = new byte[1024];
+                var sizeReceived = clientSocket.Receive(byteReceived);
+
+                Console.WriteLine("Received: {0}", Encoding.ASCII.GetString(byteReceived, 0, sizeReceived));
+
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+                
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteServer();
+        }
+
+        public static void ExecuteServer()
+        {
+            IPHostEntry hostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = hostEntry.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 11111);
+
+            Socket hostSocket = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                hostSocket.Bind(endPoint);
+
+                hostSocket.Listen(2);
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting connection ....");
+                    Socket incomingClientSocket = hostSocket.Accept();
+
+                    byte[] buffer = new byte[1024];
+                    string decodData = null;
+
+                    while (true)
+                    {
+                        int receivedSize = incomingClientSocket.Receive(buffer);
+
+                        decodData = Encoding.ASCII.GetString(buffer, 0, receivedSize);
+
+                        if (decodData.IndexOf("world") > -1)
+                            break;
+                    }
+
+                    Console.WriteLine("HOST > Received message ", decodData);
+
+                    byte[] toClientMessage = Encoding.ASCII.GetBytes("Server tested successfully");
+
+                    incomingClientSocket.Send(toClientMessage);
+
+                    incomingClientSocket.Shutdown(SocketShutdown.Both);
+                    incomingClientSocket.Close();
+                }
+            }
+            catch (SocketException ex)
+            {
+
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
